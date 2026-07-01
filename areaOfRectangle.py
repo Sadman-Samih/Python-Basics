3630354f1dc3a3c7e4d94339138c1bfe214f377d
Sadman <sami20112012000@gmail.com>
2026-07-01 18:12:01 +0600

    added float and f string properties

--- areaOfRectangle.py
+++ areaOfRectangle.py
@@ -1,6 +1,6 @@
-length = int(input("Enter the length of the rectangle: "))
-width = int(input("Enter the width of the rectangle: "))
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
 
 area = length * width
 
-print(area)
+print(f"The area of the rectangle is {area} cm")
