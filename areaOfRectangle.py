e36c500f7007bc2820adf984037abedbc7054f2a
Sadman <sami20112012000@gmail.com>
2026-07-01 18:43:25 +0600

    ig

--- areaOfRectangle.py
+++ areaOfRectangle.py
@@ -1,6 +1,8 @@
+from django.db.models.functions import window
+
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
 area = length * width
 
-print(f"The area of the rectangle is {area} cm")
+print(f"The area of the rectangle is {area} cm squared.")
