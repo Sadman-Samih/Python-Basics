ab8a7abe9a8c85e68c25f33f3acf4df65fb47fb3
Sadman <sami20112012000@gmail.com>
2026-07-01 10:43:01 +0600

    added python

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+name = "John"
+age = 30
+gpa = 3.5
+Is_student = "You are a student"
+
+
+print(f"Your name is {name}, You are {age} years old. Your Gpa is {gpa}.  {Is_student}.")
+
