b4351005792da42bb5bed038bb37abfe6471585d
Sadman <sami20112012000@gmail.com>
2026-07-02 16:27:01 +0600

    added math and if else

---
+++ logic.py
@@ -0,0 +1,26 @@
+import math
+
+#x = int(input("Enter a number: "))
+#result = math.sqrt(x)
+#print(f"The square root of {x} is {result}")
+
+#y = 34.9
+#result = math.ceil(y)
+#print(result)
+
+#radius = float(input("Please enter the radius of the circle: "))
+#circumference = 2 * math.pi * radius
+#print(f"The circumference of the circle is {circumference}")
+
+# def passport():
+   # age = int(input("Enter your age: "))
+
+    # if age >= 13:
+       # print(f"Your age is {age} years old. You are eligible for a passport.")
+   # else:
+       # print(f"Your age is {age} years old. You are not eligible for a passport.")
+
+#passport()
+
+####################################################################################
+
