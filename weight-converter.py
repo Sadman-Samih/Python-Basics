a44ee7b6762f370ed728cd33aa0d3ce48e6e9e14
Sadman <sami20112012000@gmail.com>
2026-07-04 18:32:27 +0600

    added weight converter

--- weight-converter.py
+++ weight-converter.py
@@ -2,8 +2,12 @@ weight = float(input("Enter your weight: "))
 unit = input("Enter your unit [K/L]: ")
 
 if unit == "K":
-    result = weight / 100
-    print(f"Your weight is {result}")
+    result = weight * 2.205
+    unit = "Kgs"
+    print(f"Your weight is {result} {unit}")
 elif unit == "L":
-    result = weight / 1000
-    print(f"Your weight is {result}")
+    result = weight / 2.205
+    unit = "Lbs"
+    print(f"Your weight is {result} {unit}")
+else :
+    print("Invalid input")
