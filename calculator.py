ec5aab78c7dd83d504a0847da361f54850e1395c
Sadman <sami20112012000@gmail.com>
2026-07-04 18:22:11 +0600

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+oparators = input("Enter an oparator (+, -, *, /): ")
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+if oparators == "+":
+    result = num1 + num2
+    print(f"the result is {result}")
+elif oparators == "-":
+    result = num1 - num2
+    print(f"the result is {result}")
+elif oparators == "*":
+    result = num1 * num2
+    print(f"the result is {result}")
+elif oparators == "/":
+    result = num1 / num2
+    print(f"the result is {result}")
